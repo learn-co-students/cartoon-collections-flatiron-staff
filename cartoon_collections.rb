8ae66cd3ea1e060c08ac02357c8dee36114795fa
Anna Lux <anna.watt@flatironschool.com>
2020-04-20 12:56:18 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,9 +3,7 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |calls| calls.capitalize! << "!"
-  end
-  planeteer_calls
+  planeteer_calls.collect {|calls| calls.capitalize! << "!"}
 end
 
 def long_planeteer_calls(calls)
